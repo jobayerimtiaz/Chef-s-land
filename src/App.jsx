56f661d7b6563c009f09d6a4db80eea8c49a8f1c
jobayerimtiaz <jiefq.37@gmail.com>
2024-11-22 15:54:51 +0600

    functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,10 @@ import Sidebar from "./components/Sidebar";
 
 function App() {
   const [recipeQueue, setRecipeQueue] = useState([]);
+  const [preparedRecipe, setPreparedRecipe] = useState([]);
+  const [totalTime, setTotalTime] = useState(0);
+  const [totalCalories, setTotalCalories] = useState(0);
+
   const handleRecipeQueue = (item) => {
     const isExist = recipeQueue.find(
       (previousRecipe) => previousRecipe.recipe_id === item.recipe_id
@@ -18,7 +22,20 @@ function App() {
       alert("Recipe exists");
     }
   };
-  console.log(recipeQueue);
+
+  const handleRemove = (id) => {
+    const deletedRecipe = recipeQueue.find((recipe) => recipe.recipe_id === id);
+    const updatedQueue = recipeQueue.filter(
+      (recipe) => recipe.recipe_id !== id
+    );
+    setRecipeQueue(updatedQueue);
+    setPreparedRecipe([...preparedRecipe, deletedRecipe]);
+  };
+
+  const calculateTimeAndCalories = (time, calorie) => {
+    setTotalTime(totalTime + time);
+    setTotalCalories(totalCalories + calorie);
+  };
 
   return (
     <>
@@ -27,7 +44,14 @@ function App() {
       <Recipes></Recipes>
       <section className="flex flex-col md:flex-row gap-6">
         <Items handleRecipeQueue={handleRecipeQueue}></Items>
-        <Sidebar recipeQueue={recipeQueue}></Sidebar>
+        <Sidebar
+          recipeQueue={recipeQueue}
+          handleRemove={handleRemove}
+          preparedRecipe={preparedRecipe}
+          calculateTimeAndCalories={calculateTimeAndCalories}
+          totalTime={totalTime}
+          totalCalories={totalCalories}
+        ></Sidebar>
       </section>
     </>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,37 +1,90 @@
-const Sidebar = ({ recipeQueue }) => {
+/* eslint-disable react/prop-types */
+import PropTypes from "prop-types";
+const Sidebar = ({
+  recipeQueue,
+  handleRemove,
+  preparedRecipe,
+  calculateTimeAndCalories,
+  totalTime,
+  totalCalories,
+}) => {
   return (
-    <div className="md:w-1/3 border-2 text-gray-300 rounded-2xl">
-      <h2>Name: {recipeQueue.length}</h2>
-      {recipeQueue.map((food, idx) => {
-        <p key={idx}>{food.recipe_name}</p>;
-      })}
+    <div className="md:w-1/3 border-2 text-gray-600 rounded-2xl">
+      <div className="overflow-x-auto">
+        <h2>Recipe Name : {recipeQueue.length}</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Time</th>
+              <th>Calories</th>
+            </tr>
+          </thead>
+          <tbody>
+            {recipeQueue.map((recipe, idx) => (
+              <tr key={idx}>
+                <th>{idx + 1}</th>
+                <td>{recipe.recipe_name}</td>
+                <td>{recipe.preparing_time}</td>
+                <td>{recipe.calories}</td>
+                <button
+                  className="btn btn-accent"
+                  onClick={() => {
+                    handleRemove(recipe.recipe_id);
+                    calculateTimeAndCalories(
+                      recipe.preparing_time,
+                      recipe.calories
+                    );
+                  }}
+                >
+                  Cook
+                </button>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="overflow-x-auto">
+        <h2>Cooking : {preparedRecipe.length}</h2>
+        <table className="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Time</th>
+              <th>Calories</th>
+            </tr>
+          </thead>
+          <tbody>
+            {preparedRecipe.map((recipe, idx) => (
+              <tr key={idx}>
+                <th>{idx + 1}</th>
+                <td>{recipe.recipe_name}</td>
+                <td>{recipe.preparing_time}</td>
+                <td>{recipe.calories}</td>
+              </tr>
+            ))}
+            <tr>
+              <th></th>
+              <td></td>
+              <td>Time : {totalTime}</td>
+              <td>Calorie : {totalCalories}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
 
-export default Sidebar;
+Sidebar.propTypes = {
+  recipeQueue: PropTypes
+    .arrayOf
+    // PropTypes.shape({
+    //   recipe_name: PropTypes.string.isRequired,
+    // })
+    ().isRequired,
+};
 
-// {
-//   /* <div className="overflow-x-auto">
-//   <table className="table">
-//     <thead>
-//       <tr>
-//         <th></th>
-//         <th>Name</th>
-//         <th>Time</th>
-//         <th>Calories</th>
-//       </tr>
-//     </thead>
-//     <tbody>
-//       {recipeQueue.map((recipe, idx) => {
-//         <tr key={idx}>
-//           <th>{idx + 1}</th>
-//           <td>{recipe.length}</td>
-//           <td>{recipe.preparing_time}</td>
-//           <td>{recipe.calories}</td>
-//         </tr>;
-//       })}
-//     </tbody>
-//   </table>
-// </div>; */
-// }
+export default Sidebar;
